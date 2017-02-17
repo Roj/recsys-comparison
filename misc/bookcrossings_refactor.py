5545872a1c9d1be85de6aab42bd659d75bfcd310
Roj <joa.dev@live.com>
2017-02-17 20:46:12 -0300

    Added option to cut bookcrossings data; also template for other scripts

--- misc/bookcrossings_refactor.py
+++ misc/bookcrossings_refactor.py
@@ -1,12 +1,18 @@
 #script to rename users and books(ISBNs) with different numbers
 #as they appear they are added into a dictionary with
 #increasing primary key
+#also, optionally, removes a percentage of the data
 from __future__ import division
 import csv
 
+CUT_USERS = True
+CUT_PERC = 0.3 # 0.3 == 30% of data is removed
+
 entrada = "BX-Book-Ratings.csv"
 salida = "BX-Book-Ratings_.csv"
+
 data = list()
+
 with open(entrada) as f:
 	reader = csv.reader(f,delimiter=";")
 	reader.next()
@@ -26,7 +32,11 @@ for row in data:
 		books_id+=1
 	row[0] = users[row[0]]
 	row[1] = books[row[1]]
-		
+
+if CUT_USERS:	
+	cut = len(users)*(1-CUT_PERC)
+	data = [row for row in data if row[0]<cut]
+
 
 with open(salida,"w") as f:
 	writer = csv.writer(f,delimiter=",")
